324882b9c566ad03e542c361e95c5c8db0abbb88
NickCrews <nicholas.b.crews@gmail.com>
2018-07-24 13:40:32 -0800

    little bash script to set up gpsd unix environment for python

---
+++ boat/runmain.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# start gpsd so that python can access the adafruit GPS breakout over UART.
+# There are some additional steps you must follow once, such as downloading gpsd
+# and enabling serial communication, but once done should
+# not have to be re-done. Everthing is from
+# https://learn.adafruit.com/adafruit-ultimate-gps-on-the-raspberry-pi/using-uart-instead-of-usb
+sudo systemctl stop serial-getty@ttyS0.service
+sudo systemctl disable serial-getty@ttyS0.service
+sudo killall gpsd
+sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
