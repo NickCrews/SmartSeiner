eac7fa3e071fe9e12bc7fb127e53f3d8c27c0262
NickCrews <nicholas.b.crews@gmail.com>
2018-08-03 19:42:18 -0800

    added threaded radio code

--- pi/skiff.py
+++ pi/skiff.py
@@ -1,75 +1,109 @@
-
 import numpy as np
-import atexit
+import time
+from struct import unpack
+from threading import Thread
+from queue import Queue
 
 from RFM69 import Radio, FREQ_915MHZ
-import datetime
-import time
-
-THIS_NODE_ID = 2
-NETWORK_ID = 1
-OTHER_NODE_ID = 2
-
-# with Radio(FREQ_915MHZ, THIS_NODE_ID, NETWORK_ID, isHighPower=True, verbose=True) as radio:
-#     print ("Starting loop...")
-#
-#     rx_counter = 0
-#     tx_counter = 0
-#
-#     while True:
-#
-#         # Every 10 seconds get packets
-#         if rx_counter > 10:
-#             rx_counter = 0
-#
-#             # Process packets
-#             for packet in radio.get_packets():
-#                 print (packet)
-#
-#         # Every 5 seconds send a message
-#         if tx_counter > 5:
-#             tx_counter=0
-#
-#             # Send
-#             print ("Sending")
-#             if radio.send(2, "TEST", attempts=3, waitTime=100):
-#                 print ("Acknowledgement received")
-#             else:
-#                 print ("No Acknowledgement")
-#
-#
-#         print("Listening...", len(radio.packets), radio.mode_name)
-#         delay = 0.5
-#         rx_counter += delay
-#         tx_counter += delay
-#         time.sleep(delay)
 
 import compass
+
+NETWORK_ID = 1
+THIS_NODE_ID = 1
+OTHER_NODE_ID = 2
 
 class Skiff(object):
 
     def __init__(self, compass_file=None):
         self.compass = compass.Compass()
-        atexit.register(self._cleanup)
-        # self.radio = Radio(FREQ_915MHZ,
-        #                    THIS_NODE_ID,
-        #                    NETWORK_ID,
-        #                    isHighPower=True,
-        #                    verbose=False)
         self.has_new_data = False
+        self.radio = MyRadio()
+
+    def has_data(self):
+        return self.radio.has_data()
 
     def get_data(self):
-        # self._update()
-        data = {}
-        data['lat']        = np.nan
-        data['lon']        = np.nan
-        data['datetime']   = np.nan
-        data['COG']        = np.nan
-        data['heading']    = np.nan
-        data['speed']      = np.nan
+        all_readings = self.radio.get_data()
+        print("allreading:", all_readings)
+        if len(all_readings) < 1:
+            return dict(lat=np.nan,
+                        lon=np.nan,
+                        heading=np.nan,
+                        COG=np.nan,
+                        speed=np.nan
+                        )
+        most_recent_bytes = all_readings[-1]
+        dictionary = self.parse_bytes(byte_list)
         self.has_new_data = False
-        return data
+        return dictionary
 
-    def _cleanup(self):
-        if hasattr(self, "radio") and self.radio:
-            self.radio._shutdown()
+    def parse_bytes(self, byte_list):
+        assert len(byte_list) == 8
+        lat = self.bytes2float(byte_list[0:4])
+        lat = self.bytes2float(byte_list[4:8])
+        return dict(lat=lat,
+                    lon=lon,
+                    heading=np.nan,
+                    COG=np.nan,
+                    speed=np.nan)
+
+    def bytes2float(self, byte_list):
+        assert len(byte_list) == 4
+        byte_array = bytearray(byte_list)
+        return unpack('<f', byte_arr)[0]
+
+class MyRadio(object):
+
+    def __init__(self):
+        self.q = Queue()
+        self.poller = RadioPoller(self.q)
+        self.poller.start()
+
+    def has_data(self):
+        return not self.q.empty()
+
+    def get_data(self):
+        readings = []
+        while not self.q.empty():
+            readings.append(byteself.q.get())
+        return readings
+
+class RadioPoller(Thread):
+    '''A separate thread that constantly polls the RFM69 and places data in
+    a queue, to be read by the owning MyRadio'''
+
+    def __init__(self, result_queue, sleep_time=.05):
+        Thread.__init__(self, name='rfm69_poller')
+        self.q = result_queue
+        self.sleep_time = sleep_time
+
+    def run(self):
+        with Radio(FREQ_915MHZ,
+                   THIS_NODE_ID,
+                   NETWORK_ID,
+                   isHighPower=False,
+                   verbose=False) as radio:
+
+            #set bitrate to 1.2kbps from library default of 55kbps
+            # The minimum RSSI value that gets through goes down,
+            # Demonstrating that it is more robust
+            radio._writeReg(0x03, 0x68)
+            radio._writeReg(0x04, 0x2B)
+
+            # enable pre-amp on receiver, increasing sensitivty and range
+            # to verfiy, place Tx and RX in non moving locations, and print RSSI
+            # values with this used and without it. There should be a 2-4 dB
+            # benefit from using the pre-amp
+            # lowpowerlab.com/forum/rf-range-antennas-rfm69-library/rfm69hw-range-test!
+
+            # radio._writeReg(0x58, 0x2D)
+
+            while True:
+                # print("RadioPoller is polling...")
+                for packet in radio.get_packets():
+                    # entry = (int(packet.RSSI), packet.received
+                    print("RadioPoller received some data!")
+                    self.q.put(packet.data[:])
+                    # print (packet.RSSI, bytes2floats(packet.data))
+
+                time.sleep(self.sleep_time)
